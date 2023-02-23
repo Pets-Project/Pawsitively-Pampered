4252df84809aba90ecd6cc4cebf4481c3d06ab81
Max Fell <Maxwilliamfell@gmail.com>
2023-02-23 17:36:15 -0500

    starter code for backend

--- Develope Backend/server.js
+++ Develope Backend/server.js
@@ -1,3 +1,15 @@
+//took from the previous projects as template
+
+const router = require('express').Router();
+const apiRoutes = require('./routes');
+
+router.use('/routes', apiRoutes);
+
+
+//Maxs code
+module.exports = router;
+
+
 require('dotenv').config();
 
 // get express
