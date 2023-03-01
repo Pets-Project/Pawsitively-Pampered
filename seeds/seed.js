e92018509a8d6217193edb3b2b3ace9ac424b404
syntaxsemantixs <gormeno2030@gmail.com>
2023-02-28 21:03:45 -0500

    creating post, put and delete for routes

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../configs/connection');
+const sequelize = require('../config/connection');
 const { User, } = require('../models');
 
 const userData = require('./userData.json');
